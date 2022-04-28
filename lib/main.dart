402d033cba21d9807efdff74f4760b96fa6ace1b
xuky <xuky@eastsoft.com.cn>
2022-04-28 10:48:21 +0800

    菜单字样修改

--- lib/main.dart
+++ lib/main.dart
@@ -698,7 +698,7 @@ class _HomePage3State extends State<HomePage3> {
                 buildPopupMenuItem(Icons.cast_connected, '扫一扫码', 'C'),
                 buildPopupMenuItem(Icons.keyboard, '写入xlsx文件', 'D'),
                 buildPopupMenuItem(Icons.file_copy, '读取xlsx文件', 'E'),
-                buildPopupMenuItem(Icons.devices_other, '其他功能', 'F'),
+                buildPopupMenuItem(Icons.devices_other, '其他功能-1', 'F'),
               ],
               onSelected: (String action) {
                 // 点击选项的时候
